f4dc8d9ccc4528bb079331fdb60f5df45b766e5b
Unknown <lee.david.cs@gmail.com>
2019-10-21 22:56:47 -0700

    Add mock in-memory db

--- src/mongodb/index.ts
+++ src/mongodb/index.ts
@@ -1,4 +1,5 @@
 import { Logger } from "@/utils";
+import { MongoMemoryServer } from "mongodb-memory-server";
 import Mongoose from "mongoose";
 import { UserModel } from "./models";
 
@@ -10,30 +11,37 @@ const dbName = process.env.MONGODB_DBNAME;
 const pass = process.env.MONGODB_PASS;
 const user = process.env.MONGODB_USER;
 
-interface IMongoDBConnectionParams {
-	host: string;
-	port: string;
-	dbName: string;
-	pass?: string;
-	user?: string;
-}
-
-const buildConnectionUri = (params: IMongoDBConnectionParams): string => {
-	const authPartial: string = params.pass && params.user ? `${user}:${pass}@` : "";
-	const uri: string = `mongodb://${authPartial}${host}:${port}/${dbName}`;
-
-	return uri;
-};
-
-export const connectToDatabase = async (): Promise<Mongoose.Mongoose> => {
+const getRealConnectionUri = (): string => {
 	/* tslint:disable:no-if-statement */
 	if (!host || !port || !dbName) {
 		throw new Error("Could not connect to database. Check environment variables");
 	}
 	/* tslint:enable:no-if-statement */
 
+	const authPartial: string = pass && user ? `${user}:${pass}@` : "";
+	const uri: string = `mongodb://${authPartial}${host}:${port}/${dbName}`;
+
+	return uri;
+};
+
+const getMockConnectionUri = async (): Promise<string> => {
+	const mongoServer: MongoMemoryServer = new MongoMemoryServer();
+
+	const uri: string = await mongoServer.getConnectionString();
+
+	return uri;
+};
+
+const getConnectionUri = async (): Promise<string> => {
+	const connectionUri: string =
+		process.env.NODE_ENV === "testing" ? await getMockConnectionUri() : getRealConnectionUri();
+
+	return connectionUri;
+};
+
+export const connectToDatabase = async (): Promise<Mongoose.Mongoose> => {
 	try {
-		const connectionUri: string = buildConnectionUri({ host, port, dbName, user, pass });
+		const connectionUri: string = await getConnectionUri();
 
 		await Mongoose.connect(connectionUri, {
 			useNewUrlParser: true
