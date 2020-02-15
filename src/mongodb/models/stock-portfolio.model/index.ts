d4b9abb2344858506237bbe0b745aacfd8bbf74b
leedavidcs <lee.david.cs@gmail.com>
2020-02-14 20:42:20 -0800

    Fix not exporting required properties from models

--- src/mongodb/models/stock-portfolio.model/index.ts
+++ src/mongodb/models/stock-portfolio.model/index.ts
@@ -1,7 +1,7 @@
 import { model, Model } from "mongoose";
 import { IStockPortfolio, StockPortfolioSchema } from "./schema";
 
-export { DataKeys } from "./header.schema";
+export * from "./header.schema";
 export { IStockPortfolio } from "./schema";
 
 export const StockPortfolioModel: Model<IStockPortfolio> = model<IStockPortfolio>(
