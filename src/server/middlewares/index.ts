b203f1e139ed7b05be14726762508c28e0254917
leedavidcs <lee.david.cs@gmail.com>
2020-02-25 04:39:35 -0800

    Remove auth route middleware, that seems not to do anything

--- src/server/middlewares/index.ts
+++ src/server/middlewares/index.ts
@@ -1,11 +1,10 @@
 import { Server } from "@/server";
 import { Middleware } from "koa";
-import { AuthMiddleware } from "./authentication.middleware";
 import { BodyParserMiddleware } from "./body-parser.middleware";
 import { LoggerMiddleware } from "./logger.middleware";
 
 export const applyMiddlewares = (server: Server): void => {
-	const middlewares: Middleware[] = [LoggerMiddleware, BodyParserMiddleware, AuthMiddleware];
+	const middlewares: Middleware[] = [LoggerMiddleware, BodyParserMiddleware];
 
 	middlewares.forEach((middleware) => server.app.use(middleware));
 };
